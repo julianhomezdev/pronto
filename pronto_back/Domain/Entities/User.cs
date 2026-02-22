b17684cb9dd7fb2a83bb5efda425212330b1424e
Julian Homez <julianhomesdev@icloud.com>
2026-02-22 00:48:20 -0500

    feat(domain): Add User Entity

---
+++ pronto_back/Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pronto_back.Domain.Entities
+{
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string IsActive { get; set; }
+        
+
+
+    }
+}
